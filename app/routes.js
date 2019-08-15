3f0ab56940851fafdae19d672b471122a4f388a7
Tom Rooker <tomdrooker@gmail.com>
2019-08-15 15:37:21 +0100

    New question added

--- app/routes.js
+++ app/routes.js
@@ -11,6 +11,16 @@ router.post('/juggling-balls-answer', function(req, res) {
   } else {
     res.redirect('/ineligible')
   }
+});
+
+router.post('/over-18-answer', function(req, res) {
+  let over18 = req.session.data['over-18']
+
+  if (over18 == 'Yes') {
+    res.redirect('/juggling-balls')
+  } else {
+    res.redirect('/ineligible')
+  }
 })
 
 module.exports = router
